7f57ac6ba7c96318da43261f56e256ede5211861
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2024-04-15 22:31:36 +0530

    updated code for admin and user controller

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -61,10 +61,19 @@ export const login = catchAsyncErrors(async (req, res, next) => {
 
 // to  add a new admin
 
-export const addNewAdmin = catchAsyncErrors(async(req, res, next) =>{
-  const {firstName, lastName, email, phone, password,gender,dob} =
-  req.body;
-  if(!firstName || !lastName || !email || !phone || !password || !gender || dob){
+export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
+  const { firstName, lastName, email, phone, dob, gender, password } =
+    req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+ 
+    !dob ||
+    !gender ||
+    !password
+  ) {
     return next(new ErrorHandler("Please Fill Full Form!", 400));
   }
   const isRegistered = await User.findOne({email});
@@ -75,5 +84,112 @@ export const addNewAdmin = catchAsyncErrors(async(req, res, next) =>{
 })
 
 export const getAllDoctors = catchAsyncErrors(async(req,res,next) =>{
-  const doctors = await User.find()
+  const doctors = await User.find({role : "Doctor"});
+  res.status(200).json({
+    success:true,
+    doctors
+  })
 })
+
+// to get all the details of the user
+
+
+export const getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  })
+})
+
+// to logout the admin
+
+export const logoutAdmin = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(201)
+    .cookie("adminToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Admin Logged Out Successfully.",
+    });
+});
+
+
+// Logout function for frontend patient
+export const logoutPatient = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(201)
+    .cookie("patientToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Patient Logged Out Successfully.",
+    });
+});
+
+
+export const addNewDoctor = catchAsyncErrors(async (req, res, next) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return next(new ErrorHandler("Doctor Avatar Required!", 400));
+  }
+  const { docAvatar } = req.files;
+  const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
+  if (!allowedFormats.includes(docAvatar.mimetype)) {
+    return next(new ErrorHandler("File Format Not Supported!", 400));
+  }
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    dob,
+    gender,
+    password,
+    doctorDepartment,
+  } = req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !dob ||
+    !gender ||
+    !password ||
+    !doctorDepartment ||
+    !docAvatar
+  ) {
+    return next(new ErrorHandler("Please Fill Full Form!", 400));
+  }
+
+const isRegistered = await User.findOne({ email });
+if (isRegistered) {
+  return next(
+    new ErrorHandler("Doctor With This Email Already Exists!", 400)
+  )}
+  
+  const doctor = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,  
+    dob,
+    gender,
+    password,
+    role: "Doctor",
+    doctorDepartment,
+    docAvatar: {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    },
+  });
+  res.status(200).json({
+    success: true,
+    message: "New Doctor Registered",
+    doctor,
+  });
+});
