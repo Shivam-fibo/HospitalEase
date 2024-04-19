a4b9b7454a6d40c1d8fcc93c84567c4b21248112
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2024-04-19 23:55:39 +0530

    frontend done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
-import './App.css'
-import {BrowserRouter as Router , Routes, Route} from "react-router-dom"
-import Home from './Pages/Home'
-import Appointment from "./Pages/Appointment"
-import Register from "./Pages/Register"
-import Login from "./Pages/Login"
-import AboutUs from "./Pages/AboutUs"
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Appointment from "./Pages/Appointment";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import AboutUs from "./Pages/AboutUs";
+import Navbar from "./components/Navbar";
+import "react-toastify/dist/ReactToastify.min.css";
+import { Context } from "./main";
+import { useContext } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import Footer from "./components/Footer";
 
 const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const respone = await axios.get(
+          "http://localhost:4000/api/v1/user/patient/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true)
+        setUser(respone.data.user)
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({})
+      }
+    };
+    fetchUser()
+  }, [isAuthenticated]);
   return (
     <>
-     <Router>
-       <Routes>
-         <Route path="/" element = {<Home/> }/>
-         <Route path="/appointment" element={<Appointment />} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-       </Routes>
-      </Router> 
+        </Routes>
+        <Footer/>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,53 +1,65 @@
-import axios from "axios";
-import React from "react";
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import axios from "axios";
 import { toast } from "react-toastify";
 import { Context } from "../main";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
   const { isAuthenticated, setIsAuthenticated } = useContext(Context);
-  const navigateTo = useNavigate()
-  const handleLogout = async() =>{
-      
-        await axios.get("http://localhost:4000/api/v1/user/patient/logout", {
-          withCredentials: true
-        }).then(res =>{
-          toast.success(res.data.message);
-          setIsAuthenticated(false)
-        }).catch(err =>{
-          toast.error(err.response.data.message )
-        })
-      
-  }
 
-  const gotoLogin = async() =>{
-    navigateTo("/login")
-  }
+  const handleLogout = async () => {
+    await axios
+      .get("http://localhost:4000/api/v1/user/patient/logout", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        toast.success(res.data.message);
+        setIsAuthenticated(false);
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
+  const navigateTo = useNavigate();
+
+  const goToLogin = () => {
+    navigateTo("/login");
+  };
+
   return (
-    <div>
-      <nav className="container">
-        <div className="logo">HospitalEase</div>
-        <div className={show ? "navlinks showmenu" : "navlinks"}>
+    <>
+      <nav className={"container"}>
+       
+        <div className={show ? "navLinks showmenu" : "navLinks"}>
           <div className="links">
-            <Link to={"/"}>Home</Link>
-            <Link to={"/appointment"}>Appointment</Link>
-            <Link to={"/about"}>About Us</Link>
+            <Link to={"/"} onClick={() => setShow(!show)}>
+              Home
+            </Link>
+            <Link to={"/appointment"} onClick={() => setShow(!show)}>
+              Appointment
+            </Link>
+            <Link to={"/about"} onClick={() => setShow(!show)}>
+              About Us
+            </Link>
           </div>
-
-          {isAuthenticated ? (
+          {!isAuthenticated ? (
             <button className="logoutBtn btn" onClick={handleLogout}>
-              Logout
+              LOGOUT
             </button>
           ) : (
-            <button className="logoutBtn" onClick={gotoLogin}>
-              Login
+            <button className="loginBtn btn" onClick={goToLogin}>
+              LOGIN
             </button>
           )}
         </div>
+        <div className="hamburger" onClick={() => setShow(!show)}>
+          <GiHamburgerMenu />
+        </div>
       </nav>
-    </div>
+    </>
   );
 };
 
